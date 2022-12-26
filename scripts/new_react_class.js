74c0e450a9e0fab081a46fe4aadc84d79eebf925
Zhisai Wu 吴之赛(WTCCN HO IT-IT Partner) <Zhisai.Wu@watsons.com.cn>
2022-12-26 18:36:54 +0800

    新增hooks模板

--- scripts/new_react_class.js
+++ scripts/new_react_class.js
@@ -1,6 +1,7 @@
 /**
- * 快速创建react类组件模板
- * 使用时需要传入需要生成文件的路径
+ * 快速创建小程序react组件模板
+ * 使用时需要传入需要生成文件的路径,第三个参数传了的话就是使用hooks模板
+ * node ./new_react_template.js D:/my-app/src/pages/test
  */
 
 const fs = require('fs');
@@ -9,13 +10,15 @@ if (!process.argv[2]) {
   throw "给个文件路径~"
 }
 
+const is_hooks = Boolean(process.argv[3]);
+
 let fs_name = process.argv[2].split('/')
 fs_name = fs_name[fs_name.length - 1]
 fs_name = fs_name.split('')
 fs_name[0] = fs_name[0].toUpperCase()
 fs_name = fs_name.join('').replace(/(-|_)(.)/g, (a) => a[1].toUpperCase())
 
-const obj = `
+const class_template = `
 import Taro from '@tarojs/taro'
 
 import './index.scss'
@@ -41,10 +44,28 @@ export default class ${fs_name} extends Component {
   }
 }
 `
+const hooks_template = `import Taro, { useEffect, useState } from '@tarojs/taro'
+
+import './index.scss'
+
+export default function Test(props) {
+  const { } = props
+  const [state, set_state] = useState()
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <View></View>
+  )
+}
+
+`
 try {
   // D:/my-github-apps/min-app1/src/pages/ppo
   fs.mkdirSync(process.argv[2]) // 创建文件
-  fs.writeFileSync(path.join(process.argv[2] + '/index.js'), (obj)) // 创建js文件
+  fs.writeFileSync(path.join(process.argv[2] + '/index.js'), (is_hooks ? hooks_template : class_template)) // 创建js文件
   fs.writeFileSync(path.join(process.argv[2] + '/index.scss'), "")
   console.log('创建成功！')
 } catch (error) {
